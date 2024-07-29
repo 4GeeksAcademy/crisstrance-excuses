1b3d8b7d8220c8f7a78b40894113a1cc5fb56602
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-07-29 19:23:07 +0000

    js groups

--- src/app.js
+++ src/app.js
@@ -4,8 +4,38 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Carousel } from "bootstrap";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let persona1 = {
+    name: "Cristian",
+    surname: "Lopez",
+    Car: "Mondeo"
+  };
+
+  let persona2 = {
+    name: "Cristian",
+    surname: "Lopez",
+    Car: "Polo"
+  };
+
+  let persona3 = {
+    name: "Cristian",
+    surname: "Lopez",
+    Car: "Civic"
+  };
+
+  let persona4 = {
+    name: "Cristian",
+    surname: "Lopez",
+    Car: "Caravan"
+  };
+
+  let persona5 = {
+    name: "Cristian",
+    surname: "Lopez",
+    Car: "Twingo"
+  };
+
+  console.log(persona1);
 };
